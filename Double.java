fd5e305b0abbf9e81480297c59fc8e5537f4ca01
gowri6100 <gowri610.xworkz@gmail.com>
2023-03-01 10:51:38 +0530

    second-push

---
+++ Double.java
@@ -0,0 +1,35 @@
+public class Double{
+	public static void main(String args[]){
+		
+		//double datatype 
+		
+		double my1stSTDPercenatge = 98.876545425d;
+		double my2ndSTDPercenatge = 91.87654344d;
+		double my3rdSTDPercentage = 87.7876562d;
+		double my4thSTDPercentage = 87.87634652d;
+		double my5thSTDPercentage = 86.4235672d;
+		double class9thSTDStudentPer = 34.5654332d;
+		double my8thSTDPercentageIs = 87.65d;
+		double my9thSTDPercentageIs = 76.655434d;
+		double my10thstdPercenatgeIs = 67.534666d;
+		double myDegree1stYearPercnetageIs = 81.1234d;
+		double my2ndYearDegreePercentageIs = 80.6541d;
+		
+		
+		//output of double datatype
+		
+		
+		System.out.println(my1stSTDPercenatge);
+		System.out.println("my2ndSTDPercenatge");
+		System.out.println("my3rdSTDPercentage");
+		System.out.println("my4thSTDPercentage");
+		System.out.println("my5thSTDPercentage");
+		System.out.println("class9thSTDStudentPer");
+		System.out.println("my8thSTDPercentageIs");
+		System.out.println("my9thSTDPercentageIs");
+		System.out.println("my10thstdPercenatgeIs");
+		System.out.println("myDegree1stYearPercnetageIs");
+		System.out.println("my2ndYearDegreePercentageIs");
+		
+	}
+}
---
+++ Float.java
@@ -0,0 +1,31 @@
+public class Float{
+	public static void main(String[]args){
+	
+	//Float datatype
+	
+	float  my10thSTDPercentage = 6.04f;
+	float my11thPercentage = 6.05f;
+	float myDegree1stPUPercentage = 6.23f;
+	float literacyRateInIndia = 6.42f;
+	float cutOffPercentageInOurCollege = 6.78f;
+	float eligiblePercentageInCompany = 8.99f;
+	float illiteracyRateInIndia = 8.87f;
+	float minimumPercentageToGetAdmissionInIIT = 823.76f;
+	float maximumPercentagePeopleWillAskToStudent = 8.53f;
+	float minimumKnowledgePeopleWillHaveAtCommonInIndia = 8.53f;
+	
+	
+	//output of float datatpe
+	
+	System.out.println("my10thSTDPercentage");
+	System.out.println("my11thPercentage");
+	System.out.println("myDegree1stPUPercentage");
+	System.out.println("literacyRateInIndia");
+	System.out.println("cutOffPercentageInOurCollege");
+	System.out.println("eligiblePercentageInCompany");
+	System.out.println("illiteracyRateInIndia");
+	System.out.println("minimumPercentageToGetAdmissionInIIT");
+	System.out.println("maximumPercentagePeopleWillAskToStudent");
+	System.out.println("minimumKnowledgePeopleWillHaveAtCommonInIndia");
+      }
+}
