780eda098d02d560400d4b0cd81ff20f5ffff49c
gowri6100 <gowri610.xworkz@gmail.com>
2023-03-01 11:06:01 +0530

    second-push

---
+++ Long.java
@@ -0,0 +1,32 @@
+public class Long{
+	public static void main(String args[]){
+		 
+		 //long datatype
+		 
+		 long noOFDays = 600000000;
+		 long noOfWeeks = 701232323;
+		 long noOfBagsInAirport = 50121234;
+		 long noOfHandsInFestival = 701232323;
+		 long noOfPassangersInBus = 90232323;
+		 long theWholeUniversePopulation = 60454345;
+		 long populationOfKarnataka = 70565656;
+		 long illitarcyInIndia = 6076543;
+		 long noOfUnemploymentPeopleInIndia = 70234565;
+		 long noOfWellEducatePopulationInIndia = 80234534;
+		 
+		 
+		 
+		 //output of long datatype
+		 
+		 System.out.println("noOFDays");
+		 System.out.println("noOfWeeks");
+		 System.out.println("noOfBagsInAirport");
+		 System.out.println("noOfHandsInFestival");
+		 System.out.println("noOfPassangersInBus");
+		 System.out.println("theWholeUniversePopulation");
+		 System.out.println("populationOfKarnataka");
+		 System.out.println("illitarcyInIndia");
+		 System.out.println("noOfUnemploymentPeopleInIndia");
+		 System.out.println("noOfWellEducatePopulationInIndia");
+ 	}
+}
