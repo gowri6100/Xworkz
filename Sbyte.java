f14ef566df7872b128beb828092992964393c65f
gowri6100 <gowri610.xworkz@gmail.com>
2023-03-04 23:34:03 +0530

    second-push

---
+++ Sbyte.java
@@ -0,0 +1,10 @@
+public class Sbyte{
+	public static void main(String[] args){
+    byte[] studentsMarks = {50,80,70,90,60,37,55,88,99};
+		System.out.println(studentsMarks[3]);
+		
+		for(int i=0; i<=8; i++){
+			System.out.println(studentsMarks[i]);
+		}
+	}
+}
