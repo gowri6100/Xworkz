47a9dbcd5a2a9fed01b5dec125275184f3f361b8
gowri6100 <gowri610.xworkz@gmail.com>
2023-03-07 23:25:11 +0530

    second-push

---
+++ newstring.java
@@ -0,0 +1,36 @@
+public class newstring{
+	public static void main(String[] args){
+		String animal = "lion";
+		String bird = new String ("parrot");
+		String animals = ("lion");
+		
+		System.out.println(animal==animal);
+		
+		String dance = "baratanatya";
+		String sing = new String("song");
+		String dancess = ("baratanatya");
+		
+		System.out.println(dance==dancess);
+		
+		String novel = "marana";
+		String book = new String("classmate");
+		String novels = ("marana");
+		
+		System.out.println(novel==novels);
+		
+		String cosmetic  = "mamaearth";
+		String washing = new String("sirfexal");
+		String cosmetics = ("mamaearth");
+		
+		System.out.println(cosmetic==cosmetics);
+		
+		String laptop = "lenovo";
+		String maobile =("redmi");
+		String laptops = ("lenovo");
+		
+		System.out.println(laptop==laptop);
+
+
+		}
+	
+}
