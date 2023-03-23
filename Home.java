b47dd588f61b48ee566cc568a6c4bbfd78e29033
gowri6100 <gowri610.xworkz@gmail.com>
2023-03-23 21:24:00 +0530

    second-push

---
+++ Home.java
@@ -0,0 +1,59 @@
+public class Home{
+	static String homename;
+	static int homeNum;
+	static byte noOfRooms;
+	static char firstLetterOfHomeName;
+	byte noOfWindow;
+	int price;
+	byte noOfDoor;
+	String model;
+	String cementName;
+	static byte noOfFloorItHave;
+	String paintName;
+	String theColorOfTheHome;
+	byte noOfFan;
+	final String architecture;
+	boolean isTheHomeHavePOP;
+	String graniteName;
+	int granitePrice;
+	byte noOfPeopleLiveInHome;
+	final String whereHomeWillConstruct;
+	final String materialWeUsedWhileConstructingaHome;
+	final String mandatoryForEveryLivingBeing;
+	final String haveToFollowSomeRules;
+	final int homePassword;
+	
+	
+	public static void giveShelter(){
+		
+	}
+	public void giveAComfortable(){
+		
+	}
+	public void protectFromHeavyRain(){
+		
+	}
+	public void changeTheColor(){
+		
+	}
+	public void protectFromAir(){
+		
+	}
+	public void makeConveyToAllLivingAnimal(){
+		
+	}
+	public void causingAForestDeforestation(){
+		
+	}
+	public void difficultyToLiveWithoutHome(){
+		
+	}
+	public void provideTheLivingSecurity(){
+		
+	}
+public void provideFreedom(){
+	
+}
+	
+	}
+
